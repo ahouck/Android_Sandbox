a12f66fe00e1955bb527ed875779801e45a08d3d
ahouck <ahouck@albany.edu>
2013-02-12 13:05:50 -0500

    Finished tutorial 15 and added the bin to ignore list

--- src/com/New_Boston_Tutorial/Splash.java
+++ src/com/New_Boston_Tutorial/Splash.java
@@ -1,6 +1,7 @@
 package com.New_Boston_Tutorial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splash extends Activity{
@@ -10,6 +11,28 @@ public class Splash extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.splash);
+		
+		// new thread for parallel activity 
+		Thread timer = new Thread()
+			{
+				public void run()
+				{
+					try{
+						sleep(2000);
+					}
+					catch(InterruptedException e){
+						e.printStackTrace();
+					}
+					finally{
+						Intent openStartingPoint = new Intent("com.New_Boston_Tutorial.STARTINGPOINT");
+						startActivity(openStartingPoint);
+						
+					}
+				}
+			};
+			
+			timer.start();
+			
 	}
 
 }
