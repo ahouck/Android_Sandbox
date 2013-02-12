60dce0aa0ba74c7d29ddb343b859c5995ddce5b9
ahouck <ahouck@albany.edu>
2013-02-12 17:37:07 -0500

    Setting up new activity/intent still crashing on laptop, reference tutorial #13

--- src/com/New_Boston_Tutorial/Splash.java
+++ src/com/New_Boston_Tutorial/Splash.java
@@ -33,6 +33,15 @@ public class Splash extends Activity{
 			
 			timer.start();
 			
+			
+			
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		finish();
 	}
 
 }
