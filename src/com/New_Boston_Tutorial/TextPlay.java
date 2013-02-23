5f01fba02bf97c37efc6e4be16769a393edd41fb
ahouck <ahouck@albany.edu>
2013-02-23 12:50:35 -0500

    Finished 31

--- src/com/New_Boston_Tutorial/TextPlay.java
+++ src/com/New_Boston_Tutorial/TextPlay.java
@@ -1,6 +1,9 @@
 package com.New_Boston_Tutorial;
 
+import java.util.Random;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.Gravity;
@@ -21,7 +24,7 @@ public class TextPlay extends Activity{
 		Button chkCommand = (Button) findViewById(R.id.btnResults);
 		final ToggleButton passTog = (ToggleButton) findViewById(R.id.tbPassword);
 		final EditText input = (EditText) findViewById(R.id.txtEdit);
-		final TextView display = (TextView) findViewById(R.id.btnResults);
+		final TextView display = (TextView) findViewById(R.id.txtResults);
 		
 		passTog.setOnClickListener(new View.OnClickListener() {
 			
@@ -46,16 +49,25 @@ public class TextPlay extends Activity{
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
 				String check = input.getText().toString();
-				
+				display.setText(check);
 				if(check.contentEquals("left")){
 					display.setGravity(Gravity.LEFT);
 				}else if(check.contentEquals("center")){
-					display.setGravity(Gravity.CENTER);					
+					display.setGravity(Gravity.CENTER);		
 				}else if(check.contentEquals("right")){
 					display.setGravity(Gravity.RIGHT);					
-				}else if(check.contentEquals("blue"))
-				{
+				}else if(check.contentEquals("blue")){
+					display.setTextColor(Color.BLUE);
+				}else if(check.contentEquals("wtf")){
+					Random rnd = new Random();
+					display.setText("wtf");
+					display.setTextSize(rnd.nextInt(75));
+					display.setTextColor(Color.rgb(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
 					
+					
+				}else{
+					display.setText("invalid");	
+					display.setGravity(Gravity.CENTER);
 				}
 				
 			}
