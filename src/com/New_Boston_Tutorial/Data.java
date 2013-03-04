bdc41e448627c75247893c960be489ce49f45308
ahouck <ahouck@albany.edu>
2013-03-03 20:08:38 -0500

    finished 50

--- src/com/New_Boston_Tutorial/Data.java
+++ src/com/New_Boston_Tutorial/Data.java
@@ -1,6 +1,8 @@
 package com.New_Boston_Tutorial;
 
+import android.R.string;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
@@ -26,7 +28,7 @@ public class Data extends Activity implements View.OnClickListener{
 	private void initialize(){
 		txtSend = (EditText) findViewById(R.id.txtSend);
 		btnStartAct = (Button) findViewById(R.id.btnStartAct);
-		btnStartAct = (Button) findViewById(R.id.btnStartActforResult);
+		btnStartActforResult = (Button) findViewById(R.id.btnStartActforResult);
 		txtViewGet = (TextView) findViewById(R.id.txtViewGet);
 		//On Click Listeners
 		btnStartAct.setOnClickListener(this);
@@ -35,10 +37,41 @@ public class Data extends Activity implements View.OnClickListener{
 	}
 
 	@Override
-	public void onClick(View v) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		// TODO Auto-generated method stub
-		
+		super.onActivityResult(requestCode, resultCode, data);
+		if (resultCode == RESULT_OK){
+			Bundle recievedBundle = data.getExtras();
+			txtViewGet.setText(recievedBundle.getString("key"));
+			
+		}
 	}
+
+	@Override
+	public void onClick(View v) {
+		
+		switch(v.getId()){
+		case R.id.btnStartAct:
+		String strPassing = txtSend.getText().toString();
+		Bundle bndlPackageBundle = new Bundle();
+		bndlPackageBundle.putString("key",strPassing);
+		
+		//Start up the activity
+		Intent intentOpenedClass = new Intent(Data.this, OpenedClass.class);
+		//send over the data
+		intentOpenedClass.putExtras(bndlPackageBundle);
+		startActivity(intentOpenedClass);
+		break;
+		case R.id.btnStartActforResult:
+			
+			startActivityForResult(new Intent(Data.this, OpenedClass.class),0);
+			
+		break;
+			
+		}
+	}
+	
+	
 
 }
 
--- src/com/New_Boston_Tutorial/OpenedClass.java
+++ src/com/New_Boston_Tutorial/OpenedClass.java
@@ -1,6 +1,7 @@
 package com.New_Boston_Tutorial;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,14 +17,17 @@ public class OpenedClass extends Activity implements View.OnClickListener, OnChe
 	Button btnReturn;
 	RadioGroup rdogAnsers;
 	RadioButton rdo1, rdo2, rdo3;
-	
+	String inputData, sendData;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.send);
+		
 		initialize();
-	}
+		inputData = getData();
+		txtViewQuestions.setText(inputData);
+		}
 
 	private void initialize(){
 		txtViewQuestions = (TextView) findViewById(R.id.txtViewQuestion);
@@ -37,7 +41,14 @@ public class OpenedClass extends Activity implements View.OnClickListener, OnChe
 	@Override
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
+		Intent newIntent = new Intent();
+		Bundle newBundle = new Bundle();
 		
+		newBundle.putString("key", sendData);
+		newIntent.putExtras(newBundle);
+		
+		setResult(RESULT_OK, newIntent);
+		finish();
 	}
 
 	@Override
@@ -46,12 +57,26 @@ public class OpenedClass extends Activity implements View.OnClickListener, OnChe
 		switch(checkedId)
 		{
 		case R.id.rdo1:
+			sendData="choice 1";
 			break;
 		case R.id.rdo2:
+			sendData = "choice 2";
 			break;
 		case R.id.rdo3:
+			sendData = "choice 3";
 			break;
 		}
+		txtViewTest.setText(sendData);
+	}
+	
+	private String getData(){
+		try {
+			Bundle inputBundle = getIntent().getExtras();
+			return inputBundle.getString("key");
+		} catch (Exception e) {
+			return "";
+		}
+
 	}
 
 }
