24590ecb86c4b3ca6db3ad72418d50dbb95e6823
ahouck <ahouck@albany.edu>
2013-03-11 19:50:42 -0400

    finished 64

--- src/com/New_Boston_Tutorial/AnimationClass.java
+++ src/com/New_Boston_Tutorial/AnimationClass.java
@@ -3,6 +3,8 @@ package com.New_Boston_Tutorial;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class AnimationClass extends View {
@@ -15,6 +17,17 @@ public class AnimationClass extends View {
 		//Just ising the picture I used for the button
 		pic = BitmapFactory.decodeResource(getResources(), R.drawable.btn);
 		
+		
+		
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+		
+		canvas.drawColor(Color.RED);
+		canvas.drawBitmap(pic, canvas.getWidth()/2,canvas.getHeight()/2, null);
 		
 		
 	}
