d49976fdc784572e1ee9ee5ed9656513eb24d203
ahouck <ahouck@albany.edu>
2013-03-12 11:50:41 -0400

    finished 65

--- src/com/New_Boston_Tutorial/AnimationClass.java
+++ src/com/New_Boston_Tutorial/AnimationClass.java
@@ -5,18 +5,22 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.View;
 
 public class AnimationClass extends View {
 
 	Bitmap pic;
+	float changingY;
+	Rect middleRect = new Rect();
 	
 	public AnimationClass(Context context) {
 		super(context);
 		
 		//Just ising the picture I used for the button
 		pic = BitmapFactory.decodeResource(getResources(), R.drawable.btn);
-		
+		changingY = 0;
 		
 		
 	}
@@ -26,10 +30,21 @@ public class AnimationClass extends View {
 		// TODO Auto-generated method stub
 		super.onDraw(canvas);
 		
-		canvas.drawColor(Color.RED);
-		canvas.drawBitmap(pic, canvas.getWidth()/2,canvas.getHeight()/2, null);
+		canvas.drawColor(Color.WHITE);
+		canvas.drawBitmap(pic, canvas.getWidth()/2,changingY, null);
 		
-		
+		if (changingY < (canvas.getHeight() - 10)) {
+			changingY += 5;
+		}
+		else {
+			changingY = 0;
+		}
+
+		middleRect.set(0, 400 ,canvas.getWidth(),500);
+		Paint rectBlue = new Paint();
+		rectBlue.setColor(Color.BLUE);
+		canvas.drawRect(middleRect, rectBlue);
+		invalidate();
 	}
 
 }
