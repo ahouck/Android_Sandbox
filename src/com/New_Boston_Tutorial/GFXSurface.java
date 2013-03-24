073db490b887b2fd7a708b7aa3106c25b229ca6b
ahouck <ahouck@albany.edu>
2013-03-24 15:35:25 -0400

    Changes

--- src/com/New_Boston_Tutorial/GFXSurface.java
+++ src/com/New_Boston_Tutorial/GFXSurface.java
@@ -1,8 +1,14 @@
 package com.New_Boston_Tutorial;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
@@ -50,6 +56,64 @@ public class GFXSurface extends Activity implements OnTouchListener{
 		
 	}
 	
-	
+	public class SurfaceAnimation extends SurfaceView implements Runnable{
+
+		SurfaceHolder surfaceHolder;
+		Thread animationThread;
+		boolean isRunning = false;
+		
+		public SurfaceAnimation(Context context) {
+			super(context);
+			
+			surfaceHolder = getHolder();
+			//"this" refering to having it use our run method below
+
+		}
+
+		@Override
+		public void run() {
+			
+			while(isRunning){
+				if(!surfaceHolder.getSurface().isValid())
+				{
+					continue;
+				}
+
+				Canvas canvas = surfaceHolder.lockCanvas();
+				canvas.drawRGB(80, 80, 80);
+				if( x != 0 && y !=0){
+					Bitmap tester = BitmapFactory.decodeResource(getResources(), R.drawable.btn);
+					canvas.drawBitmap(tester, x,y, null);
+				}
+				surfaceHolder.unlockCanvasAndPost(canvas);
+				 
+			}
+		}
+		
+		public void pause(){
+			isRunning = false;
+			while(true)
+			{
+				try {
+					animationThread.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				break;
+			}
+			
+			animationThread = null;
+
+		}
+
+		public void resume(){
+			isRunning = true;
+			animationThread = new Thread(this);
+			animationThread.start();
+		}
+		
+	}
+
 
 }
