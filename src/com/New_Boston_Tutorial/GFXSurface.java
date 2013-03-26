ba64afd2a9c0c890989717f6728b67b885f24b0e
ahouck <ahouck@albany.edu>
2013-03-26 18:45:52 -0400

    halfway through 73

--- src/com/New_Boston_Tutorial/GFXSurface.java
+++ src/com/New_Boston_Tutorial/GFXSurface.java
@@ -12,10 +12,10 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.View.OnTouchListener;
 
-public class GFXSurface extends Activity implements OnTouchListener{
-	
+public class GFXSurface extends Activity implements OnTouchListener {
+
 	SurfaceAnimation ourSurfaceView;
-	float x,y;
+	float x, y, sX, sY, fX, fY;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,77 +23,78 @@ public class GFXSurface extends Activity implements OnTouchListener{
 		super.onCreate(savedInstanceState);
 		x = 0;
 		y = 0;
+		sX = 0;
+		sY = 0;
+		fX = 0;
+		fY = 0;
+
 		ourSurfaceView = new SurfaceAnimation(this);
 		ourSurfaceView.setOnTouchListener(this);
 		setContentView(ourSurfaceView);
 	}
-
 
 	@Override
 	protected void onPause() {
 		// TODO Auto-generated method stub
 		super.onPause();
 		ourSurfaceView.pause();
-		
-	}
 
+	}
 
 	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		
+
 		ourSurfaceView.resume();
 	}
-
 
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		x = event.getX();
 		y = event.getY();
-		return false;
-		
-		
+		return true;
+
 	}
-	
-	public class SurfaceAnimation extends SurfaceView implements Runnable{
+
+	public class SurfaceAnimation extends SurfaceView implements Runnable {
 
 		SurfaceHolder surfaceHolder;
 		Thread animationThread;
 		boolean isRunning = false;
-		
+
 		public SurfaceAnimation(Context context) {
 			super(context);
-			
+
 			surfaceHolder = getHolder();
-			//"this" refering to having it use our run method below
+			// "this" refering to having it use our run method below
 
 		}
 
 		@Override
 		public void run() {
-			
-			while(isRunning){
-				if(!surfaceHolder.getSurface().isValid())
-				{
+
+			while (isRunning) {
+				if (!surfaceHolder.getSurface().isValid()) {
 					continue;
 				}
 
 				Canvas canvas = surfaceHolder.lockCanvas();
 				canvas.drawRGB(80, 80, 80);
-				if( x != 0 && y !=0){
-					Bitmap tester = BitmapFactory.decodeResource(getResources(), R.drawable.btn);
-					canvas.drawBitmap(tester, x,y, null);
+				if (x != 0 && y != 0) {
+					Bitmap tester = BitmapFactory.decodeResource(
+							getResources(), R.drawable.btn);
+					canvas.drawBitmap(tester, (x - (tester.getWidth() / 2)),
+							(y - (tester.getHeight() / 2)), null);
 				}
 				surfaceHolder.unlockCanvasAndPost(canvas);
-				 
+
 			}
 		}
-		
-		public void pause(){
+
+		public void pause() {
 			isRunning = false;
-			while(true)
-			{
+			while (true) {
 				try {
 					animationThread.join();
 				} catch (InterruptedException e) {
@@ -102,18 +103,17 @@ public class GFXSurface extends Activity implements OnTouchListener{
 				}
 				break;
 			}
-			
+
 			animationThread = null;
 
 		}
 
-		public void resume(){
+		public void resume() {
 			isRunning = true;
 			animationThread = new Thread(this);
 			animationThread.start();
 		}
-		
-	}
 
+	}
 
 }
